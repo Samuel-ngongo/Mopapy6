d68fd37121600b3e9b0b9d8c82147fb7d47dfcce
Samuel-ngongo <ngongosamuel00@gmail.com>
2025-05-12 08:41:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+# Versão aprimorada com IA adaptativa
+# - Lógica curta (últimas 3-7 jogadas)
+# - Análise de transições
+# - Estimativa de intervalo inferior/médio
+# - Alertas inteligentes
+print('Previsão Inteligente Ativa')
